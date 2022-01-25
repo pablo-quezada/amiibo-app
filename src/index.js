0f7a2191ca691b5154c49b363ebfebd69f7e6095
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-25 19:54:06 -0300

    add import-jquery,bootstrap and popper

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+
 
 
 var element = <div>Amiibo-app</div>;
