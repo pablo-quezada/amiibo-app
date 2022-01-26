1e5a682b17bd9b1525d926d11c265b6f426b0b03
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-26 19:02:34 -0300

    Replace appTitle to pageTitle

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,11 +2,12 @@ import React, {Component} from "react";
 
 export default class MainContent
 extends Component {
-    state = {appTitle: "Customers"};
+    state = {pageTitle: "Customers"};
 
     render() {
         return <div>
-            <h4 className="border-bottom m-1 p-1">{this.state.appTitle}</h4>
+            <h4 className="border-bottom m-1 p-1">
+                {this.state.pageTitle}</h4>
             </div>;
     }
 }
