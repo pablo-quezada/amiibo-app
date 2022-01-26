8d219fb5f25b3351f8b362252d9a00b029cc8d83
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-26 19:18:09 -0300

    Add customerCount

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,12 +2,17 @@ import React, {Component} from "react";
 
 export default class MainContent
 extends Component {
-    state = {pageTitle: "Customers"};
+    state = {pageTitle: "Customers", customersCount: 6};
 
     render() {
-        return <div>
-            <h4 className="border-bottom m-1 p-1">
-                {this.state.pageTitle}</h4>
-            </div>;
+        return ( <div>
+                    <h4 className="border-bottom m-2 p-2">
+                        {this.state.pageTitle}
+                        <span className="badge badge-danger m-2">
+                            {this.state.customersCount}
+                        </span>
+                    </h4>
+                </div>
+               );
     }
 }
