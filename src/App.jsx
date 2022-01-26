9970852c85ba0e664765d1ffda07c15013a75966
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-25 23:40:11 -0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import React, {Component} from "react";
 
-export default class NavBar extends Component {
+class NavBar extends Component {
     render() {
         return (
-            <div>
-                <h1>Shop</h1>
-                <p>Mario</p>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Amiibo-Shop</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                    </li>
+
+                    
+                       
+                    </ul>
+                    
+                </div>
             </div>
+        </nav>    
+            
         );
-  };
+    };
 }
 
 export default NavBar;
