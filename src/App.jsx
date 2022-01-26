35c89faaea0b8df16d491454484199d9fb2e2b8c
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-26 00:06:11 -0300

    add to className to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,25 @@ import React, {Component} from "react";
 class NavBar extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Amiibo-Shop</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/#">Amiibo-Shop</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
-                    </li>
-
-                    
-                       
-                    </ul>
-                    
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            
+                            <li className="nav-item">
+                            <a className="nav-link" href="/#">Home</a>
+                            </li>   
+                        </ul>   
+                    </div>
                 </div>
-            </div>
-        </nav>    
-            
+           </nav>                
         );
     };
-}
+};
 
 export default NavBar;
