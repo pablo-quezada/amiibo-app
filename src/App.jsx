fd95cebe4dc630ad699c5735dbb2249447ef380b
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-26 19:00:58 -0300

    Add state appTitle to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import MainContent from "./MainContent";
 import NavBar from "./NavBar";
-import MainContent from "./MainContent";
 
 export default class App extends
 Component
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,7 +2,11 @@ import React, {Component} from "react";
 
 export default class MainContent
 extends Component {
+    state = {appTitle: "Customers"};
+
     render() {
-        return <div>Mario, Zelda, Yoshi</div>;
+        return <div>
+            <h4 className="border-bottom m-1 p-1">{this.state.appTitle}</h4>
+            </div>;
     }
 }
