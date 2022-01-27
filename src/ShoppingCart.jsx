1a04df49e40e699fd27bfb76587e3e7c7a86a94f
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-27 18:22:53 -0300

    Add button buy now to ShoppingCart

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -21,7 +21,9 @@ export default class ShoppingCart extends Component {
 
             <div className="row">
                 {this.state.products.map((prod) => {
-                    return <Product key={prod.id} product={prod}/>;
+                    return <Product key={prod.id} product={prod}>
+                    <button className="btn btn-primary">Buy Now</button>
+                    </Product>;
                     
                 })}
             </div>
