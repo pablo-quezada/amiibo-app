15a585f6f955561d6c4e4a780a66cbd805724b99
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-27 20:12:18 -0300

    Add max value and min value to Product.jsx

--- src/Product.jsx
+++ src/Product.jsx
@@ -33,8 +33,8 @@ export default class Product extends Component {
                             <span className="badge">{this.props.product.quantity}</span>
                             
                             <div className="btn-group">
-                                <button className="btn btn-outline-success" onClick={() => {this.props.onIncrement(this.state.product);}}>+</button>
-                                <button className="btn btn-outline-success" onClick={() => {this.props.onDecrement(this.state.product);}}>-</button>
+                                <button className="btn btn-outline-success" onClick={() => {this.props.onIncrement(this.state.product, 100000);}}>+</button>
+                                <button className="btn btn-outline-success" onClick={() => {this.props.onDecrement(this.state.product, 0);}}>-</button>
                             </div>
                         </div>
 
