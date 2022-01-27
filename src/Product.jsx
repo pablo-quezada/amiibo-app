4961e35cf8048d3a8eabf624c40845e682fa3f04
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-27 19:14:42 -0300

    Add event onClick

--- src/Product.jsx
+++ src/Product.jsx
@@ -33,8 +33,8 @@ export default class Product extends Component {
                             <span className="badge">{this.props.product.quantity}</span>
                             
                             <div className="btn-group">
-                                <button className="btn btn-outline-success">+</button>
-                                <button className="btn btn-outline-success">-</button>
+                                <button className="btn btn-outline-success" onClick={this.props.onIncrement}>+</button>
+                                <button className="btn btn-outline-success" onClick={this.props.onDecrement}>-</button>
                             </div>
                         </div>
 
