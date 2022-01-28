8d014d02696e6493de48373a6420bf3ea1fed3cc
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-28 19:20:08 -0300

    Add classBootstrap to Login.jsx

---
+++ src/Login.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+export default class Login extends React.Component {
+    render () {
+        return (<div className="col-lg-9">
+            <h4 className="m-1 p-2 border-bottom">Login</h4>
+        </div>
+        );
+    }
+}
