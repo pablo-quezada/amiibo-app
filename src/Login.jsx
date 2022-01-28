dec03d081474fa22f55f48022600cc5aab3bb743
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-28 19:43:09 -0300

    Add value to email and password in Login

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,19 +5,19 @@ export default class Login extends Component {
         super(props);
         this.state = { email: "", password: "" };
     }
-    
+
     render () {
         return (<div className="col-lg-9">
             <h4 className="m-1 p-2 border-bottom">Login</h4>
 
             <div className="form-group form-row">
                 <label className="col-lg-4">Email:</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" value={this.state.email}/>
             </div>
 
             <div className="form-group form-row">
                 <label className="col-lg-4">Password:</label>
-                <input type="password" className="form-control" />
+                <input type="password" className="form-control" value={this.state.password} />
             </div>
         </div>
         );
