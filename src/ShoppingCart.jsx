6215bb122df71b7d50018402643b3f4b4e9b8b2c
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-28 16:44:45 -0300

    Add super constructor method to ShoppingCart

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -2,16 +2,23 @@ import React, {Component} from "react";
 import Product from "./Product";
 
 export default class ShoppingCart extends Component {
-    state = {
-        products: [
-            {id:1, productName: "Mario", price: 8900, quantity: 0},
-            {id:2, productName: "Zelda", price: 7300, quantity: 0},
-            {id:3, productName: "Chibi", price: 3000, quantity: 0},
-            {id:4, productName: "Robo", price: 5500, quantity: 0},
-            {id:5, productName: "Mewtoo", price: 9900, quantity: 0},
-            {id:6, productName: "Yoshi", price: 8600, quantity: 0},
-        ],
-    };
+
+    constructor() {
+        super();
+        this.state = {
+            products: [
+                {id:1, productName: "Mario", price: 8900, quantity: 0},
+                {id:2, productName: "Zelda", price: 7300, quantity: 0},
+                {id:3, productName: "Chibi", price: 3000, quantity: 0},
+                {id:4, productName: "Robo", price: 5500, quantity: 0},
+                {id:5, productName: "Mewtoo", price: 9900, quantity: 0},
+                {id:6, productName: "Yoshi", price: 8600, quantity: 0},
+            ],
+        };
+    }
+
+
+  
 
 
     render () {
