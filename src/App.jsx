2a1785ec2748af7f08681b4b18cb375310920a43
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-28 19:23:23 -0300

    Comment ShoppingCart

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import CustomerList from "./CustomerList";
+//import CustomerList from "./CustomerList";
 import Login from "./Login";
 import NavBar from "./NavBar";
-import ShoppingCart from "./ShoppingCart";
+//import ShoppingCart from "./ShoppingCart";
 
 export default class App extends
 Component
@@ -12,7 +12,6 @@ Component
         return (<React.Fragment>
                     <NavBar/>
                     <Login></Login>
-                    <ShoppingCart x="10"/>
                </React.Fragment>
                );
     }
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-export default class Login extends React.Component {
+export default class Login extends Component {
     render () {
         return (<div className="col-lg-9">
             <h4 className="m-1 p-2 border-bottom">Login</h4>
