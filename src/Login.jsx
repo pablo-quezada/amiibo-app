80d5e2cca7b8cfc015ff0fe320f170604c77d87d
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-28 19:27:16 -0300

    Add form-group Email to Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,6 +4,11 @@ export default class Login extends Component {
     render () {
         return (<div className="col-lg-9">
             <h4 className="m-1 p-2 border-bottom">Login</h4>
+
+            <div className="form-group form-row">
+                <label className="col-lg-4">Email:</label>
+                <input type="text" className="form-control" />
+            </div>
         </div>
         );
     }
