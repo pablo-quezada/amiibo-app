acb12327aea1f7dfe781c416efe0f5e14b0c4ff9
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-29 17:20:48 -0300

    Add react-router-dom and routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
 import Dashboard from "./Dashboard";
-//import CustomerList from "./CustomerList";
+import CustomerList from "./CustomerList";
 import Login from "./Login";
 import NavBar from "./NavBar";
 import ShoppingCart from "./ShoppingCart";
-
+import { Route } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 export default class App extends
 Component
 {
     render()
     {
-        return (<React.Fragment>
+        return (<BrowserRouter>
                     <NavBar/>
-                    <Dashboard></Dashboard>
-               </React.Fragment>
+                    <Route path="/" exact component={Login}/>
+                    <Route path="/dashboard" exact component={Dashboard}/>
+                    <Route path="/customers" exact component={CustomerList}/>
+                    <Route path="/cart" exact component={ShoppingCart}/>
+               </BrowserRouter>
                );
     }
     
