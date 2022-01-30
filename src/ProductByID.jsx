3b4147ca308f658072ec9eb0b73799b6955d74ec
Pablo Quezada <pablo.quezada@pjchile.com>
2022-01-30 20:04:00 -0300

    Add to navbar items to ProductbyId

---
+++ src/ProductByID.jsx
@@ -0,0 +1,69 @@
+import React, {Component} from "react";
+
+export default class ProductByID extends Component {
+    constructor(props) {
+        super(props);
+
+       // console.log("Constructor - Product");
+
+        this.state = {
+            product: this.props.product,
+        };
+    }
+  
+
+
+    render (){
+        return (
+            <div className="col-lg-6">
+                  <div className="card m-2">
+
+                <div className="card-body">
+                        <div className="text-muted"> 
+                        # {this.state.product.id}
+
+                        <span className="pull-right hand-icon" onClick={() => {
+                            this.props.onDelete(this.state.product);
+                        }}>
+                            <i className="fa fa-times"></i>
+                        </span>
+                        </div>
+
+                            <h5 className="p-2 border-top">
+                                {this.state.product.productName}
+                            </h5>
+
+                        <div>${this.state.product.price}</div>
+                        </div>
+                    {/*card body ends here */}
+
+                    <div className="card-footer">
+                        <div className="float-left">
+                            <span>{this.state.product.quantity}</span>
+                            
+                            <div className="btn-group">
+                                <button className="btn btn-outline-success" onClick={() => {this.props.onIncrement(this.state.product, 10);}}>+</button>
+                                <button className="btn btn-outline-success" onClick={() => {this.props.onDecrement(this.state.product, 0);}}>-</button>
+                            </div>
+                        </div>
+
+
+                        <div className="float-right">
+                            <Link/>
+                            {this.props.children}</div>
+                        </div>
+                    </div> 
+             </div>
+            
+          
+
+        );
+        
+    }
+
+
+
+    componenDidMount() {
+        console.log("componentDidMount - Product");
+    }
+}
